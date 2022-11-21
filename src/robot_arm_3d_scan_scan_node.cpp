34f8ef256402bf8d25bc8dea9456bcf72b726163
CarolinePascal <caroline8.pascal@gmail.com>
2022-11-21 16:52:21 +0100

    Adding a prototyped adaptive collision sphere for scanned object

--- src/robot_arm_3d_scan_scan_node.cpp
+++ src/robot_arm_3d_scan_scan_node.cpp
@@ -12,6 +12,100 @@
 
 #include <cmath>
 
+#include <pcl/io/pcd_io.h>
+#include <pcl/common/centroid.h>
+#include <pcl/common/common.h>
+
+#include <pcl_ros/point_cloud.h>
+#include <tf2_ros/transform_listener.h>
+
+class CollisionUpdater
+{   
+    public:
+        CollisionUpdater(double radiusObject, geometry_msgs::Pose objectPose) : m_radiusObject(radiusObject), m_objectPose(objectPose), m_tfListener(m_tfBuffer)
+        {
+            //Add initial collision object
+            if(radiusObject != 0)
+            {
+                m_visualTools.addSphere("collisionSphere", objectPose, radiusObject, false);
+            }
+
+            sensor_msgs::PointCloud2ConstPtr tmpPointCloud = ros::topic::waitForMessage<sensor_msgs::PointCloud2>("/point_cloud");
+            m_pointCloudFrame = tmpPointCloud->header.frame_id;
+        }
+
+        void updateCollisions(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr& pointCloud)
+        {
+            m_visualTools.deleteObject("collisionSphere");
+
+            //Compute new centroid
+            pcl::PointXYZRGB newCentoid;
+            computeCentroid(*pointCloud,newCentoid);
+
+            //Compute new bounding sphere
+            double newBoundingRadius;
+            pcl::PointXYZRGB minPoint, maxPoint;
+            pcl::getMinMax3D(*pointCloud, minPoint, maxPoint);
+
+            newBoundingRadius = sqrt((maxPoint.x - minPoint.x)*(maxPoint.x - minPoint.x) + 
+                                     (maxPoint.y - minPoint.y)*(maxPoint.y - minPoint.y) +
+                                     (maxPoint.z - minPoint.z)*(maxPoint.z - minPoint.z));    
+
+            //Get current point cloud frame position
+            tf2::Transform transform;
+            try
+            {
+                tf2::fromMsg(m_tfBuffer.lookupTransform(m_pointCloudFrame, "world", ros::Time(0), ros::Duration(5.0)).transform, transform);
+            } 
+            catch (tf2::TransformException &ex) 
+            {
+                throw std::runtime_error("CANNOT RETRIVE SEEKED TRANSFORM !");
+            }      
+
+            tf2::Matrix3x3 rotationMatrix = transform.getBasis();
+            tf2::Vector3 delta;
+
+            //TODO Some intelligent probabilities for epsilon
+            double epsilonX,epsilonY,epsilonZ;
+            epsilonX = 1.0;
+            epsilonY = 1.0;
+            epsilonZ = 0.0;
+
+            delta.setX((newCentoid.x - m_objectPose.position.x));
+            delta.setY((newCentoid.y - m_objectPose.position.y));
+            delta.setZ((newCentoid.z - m_objectPose.position.z));
+
+            delta = rotationMatrix*delta;
+
+            delta.setX(delta.x()*epsilonX);
+            delta.setY(delta.y()*epsilonY);
+            delta.setZ(delta.z()*epsilonZ);
+
+            delta = rotationMatrix.inverse()*delta;
+
+            m_objectPose.position.x += delta.x();
+            m_objectPose.position.y += delta.y();
+            m_objectPose.position.z += delta.z();
+
+            if(newBoundingRadius >= 0.75*m_radiusObject)
+            {
+                m_radiusObject = newBoundingRadius;
+            }
+
+            m_visualTools.addSphere("collisionSphere", m_objectPose, m_radiusObject, false);
+        }
+
+    private: 
+        RobotVisualTools m_visualTools;
+        double m_radiusObject;
+        geometry_msgs::Pose m_objectPose;
+
+        tf2_ros::Buffer m_tfBuffer;
+        tf2_ros::TransformListener m_tfListener;
+
+        std::string m_pointCloudFrame;
+};
+
 int main(int argc, char **argv)
 {
     //ROS node initialisation
@@ -23,22 +117,19 @@ int main(int argc, char **argv)
     //Robot initialisation TODO More generic approach
     Robot robot;
 
-    //Robot visual tools initialisation
-    RobotVisualTools visualTools;
-
     //Move the robot to its initial configuration
     //robot.init();
     robot.setAcceleration(0.05);
     robot.setVelocity(0.1);
 
     //Get the object radius, pose and the trajectory radius
-    std::vector<double> poseObject;
+    std::vector<double> rawObjectPose;
     double radiusObject;
     double radiusTrajectory;
     int trajectoryStepsNumber;
 
     ros::NodeHandle n;
-    if(!n.getParam("poseObject",poseObject))
+    if(!n.getParam("rawObjectPose",rawObjectPose))
     {
         ROS_ERROR("Unable to retrieve measurements reference pose !");
         throw std::runtime_error("MISSING PARAMETER");
@@ -62,23 +153,25 @@ int main(int argc, char **argv)
         throw std::runtime_error("MISSING PARAMETER");
     }
 
-    geometry_msgs::Pose centerPose;
-    centerPose.position.x = poseObject[0];
-    centerPose.position.y = poseObject[1];
-    centerPose.position.z = poseObject[2];
-    
-    //Add collisions objects
-    if(radiusObject != 0)
-    {
-        visualTools.addSphere("collisionSphere", centerPose, radiusObject, false);
-    }
+    geometry_msgs::Pose poseObject;
+    poseObject.position.x = rawObjectPose[0];
+    poseObject.position.y = rawObjectPose[1];
+    poseObject.position.z = rawObjectPose[2];
 
+    //Robot visual tools initialisation
+    CollisionUpdater collisionUpdater(radiusObject,poseObject);
+    ros::Subscriber collisionUpdaterSubscriber = n.subscribe("/filtered_point_cloud", 1, &CollisionUpdater::updateCollisions, &collisionUpdater);
+    
     //Create spherical scanning waypoints poses
     std::vector<geometry_msgs::Pose> waypoints;
     
-    sphericInclinationTrajectory(centerPose, radiusTrajectory, M_PI/3.5, 0, 2*M_PI, trajectoryStepsNumber, waypoints); 
-
+    //Initial measurement
+    sphericInclinationTrajectory(poseObject, radiusTrajectory*2, 0, 0, 2*M_PI, 1, waypoints); 
+    robot.runMeasurementRountine(waypoints,false,false,M_PI/2);
+    
     //Main loop
+    waypoints.clear();
+    sphericInclinationTrajectory(poseObject, radiusTrajectory, M_PI/3.5, 0, 2*M_PI, trajectoryStepsNumber, waypoints); 
     robot.runMeasurementRountine(waypoints,false,true,M_PI);
 
     //Shut down ROS node 
