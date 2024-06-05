db666c69cbdea5b3008a560af93f62da0fc8c4f5
pascal.2020 <caroline.pascal.2020@ensta-paris.fr>
2024-06-05 16:47:56 +0200

    Improving ICP and normals computation, integrating offset mesh and collision mesh in surface reconstruction

--- scripts/ICPProcessingOffline.py
+++ scripts/ICPProcessingOffline.py
@@ -17,7 +17,7 @@ def draw_registration_result(source, target, transformation):
 estimation = o3d.pipelines.registration.TransformationEstimationPointToPoint()
 criteria = o3d.pipelines.registration.ICPConvergenceCriteria(relative_fitness=0.000001, relative_rmse=0.000001, max_iteration=100)
 
-max_correspondence_distance = 0.004
+max_correspondence_distance = 0.005
 init_source_to_target = np.eye(4)
 
 if __name__ == "__main__":
@@ -25,11 +25,14 @@ if __name__ == "__main__":
     # vis = o3d.visualization.Visualizer()
     # vis.create_window()
 
-    files = sorted(glob.glob("PointCloud_*.pcd"), key = lambda file : int(file.split(".")[0].split("_")[-1]))
+    files = sorted(glob.glob("PointCloud_VP(4)_DS(0)_*.pcd"), key = lambda file : int(file.split(".")[0].split("_")[-1]))[1:]
 
     final_point_cloud = o3d.io.read_point_cloud(files[0])
-    final_point_cloud = final_point_cloud.voxel_down_sample(voxel_size=0.0025)   
+    final_point_cloud = final_point_cloud.voxel_down_sample(voxel_size=0.0025)  
+
     final_point_cloud.remove_statistical_outlier(nb_neighbors=20, std_ratio=2.0)
+    final_point_cloud.remove_radius_outlier(nb_points=100, radius=0.05)
+    
     final_point_cloud.estimate_normals(o3d.geometry.KDTreeSearchParamKNN(knn=30))
     final_point_cloud.orient_normals_consistent_tangent_plane(100)
     # vis.add_geometry(final_point_cloud)
@@ -37,8 +40,12 @@ if __name__ == "__main__":
     for i in range(len(files)-1):
         target = copy.deepcopy(final_point_cloud)
         source = o3d.io.read_point_cloud(files[i+1])
+
         source = source.voxel_down_sample(voxel_size=0.0025)
+
         source.remove_statistical_outlier(nb_neighbors=20, std_ratio=2.0)
+        source.remove_radius_outlier(nb_points=100, radius=0.05)
+
         source.estimate_normals(o3d.geometry.KDTreeSearchParamKNN(knn=30))
         source.orient_normals_consistent_tangent_plane(100)
         
@@ -75,8 +82,36 @@ if __name__ == "__main__":
         # vis.update_geometry(final_point_cloud)
         # vis.poll_events()
         # vis.update_renderer()
-        
+
+    o3d.visualization.draw_geometries([final_point_cloud])
+
+    labels = np.array(final_point_cloud.cluster_dbscan(eps=0.01, min_points=20))
+    
+    # import matplotlib.pyplot as plt
+    # max_label = labels.max()
+    # print(f"point cloud has {max_label + 1} clusters")
+    # colors = plt.get_cmap("tab20")(labels / (max_label if max_label > 0 else 1))
+    # colors[labels < 0] = 0
+    # final_point_cloud.colors = o3d.utility.Vector3dVector(colors[:, :3])
+    # o3d.visualization.draw_geometries([final_point_cloud])
+
+    final_point_cloud = final_point_cloud.select_by_index(np.where(labels == 0)[0])
+
+    # final_point_cloud = final_point_cloud.voxel_down_sample(voxel_size=0.005)
+    # final_point_cloud.remove_statistical_outlier(nb_neighbors=40, std_ratio=2.0)
+    # final_point_cloud.remove_radius_outlier(nb_points=200, radius=0.05)
+
+    final_point_cloud.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.01, max_nn=30))
+    final_point_cloud.orient_normals_consistent_tangent_plane(50) 
+
+    centroid = final_point_cloud.get_center()
+    for i,(point,normal) in enumerate(zip(final_point_cloud.points,final_point_cloud.normals)):
+        delta = point-centroid
+        delta = delta/np.linalg.norm(delta)
+        if(np.dot(normal,delta) < -0.5):
+            final_point_cloud.normals[i] = -normal
+    
     o3d.visualization.draw_geometries([final_point_cloud])
     o3d.io.write_point_cloud("PointCloudICP.pcd", final_point_cloud)
-    
+    o3d.io.write_point_cloud("PointCloudICP.xyzn", final_point_cloud)
 
--- scripts/SurfaceReconstruction.py
+++ scripts/SurfaceReconstruction.py
@@ -1,20 +1,189 @@
 import open3d as o3d
-import matplotlib.pyplot as plt
+from meshlib import mrmeshpy
+import meshlib.mrmeshnumpy as mrmeshnumpy
+import pymeshfix
+
+import trimesh
+import meshio
+
+import coacd
 import numpy as np
-import copy
 
 if __name__ == "__main__":
     point_cloud = o3d.io.read_point_cloud("PointCloudICP.pcd")
-    point_cloud = point_cloud.voxel_down_sample(voxel_size=0.005)
-    point_cloud.estimate_normals(o3d.geometry.KDTreeSearchParamKNN(knn=30))
-    point_cloud.orient_normals_consistent_tangent_plane(50)   
+
+    ### SURFACE RECONSTRUCTION ###
+
+    #mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(point_cloud, depth=10)
     
-    mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(point_cloud, depth=5)
+    mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(point_cloud, 0.02)
+    mesh.compute_vertex_normals()
+    vclean, fclean = pymeshfix.clean_from_arrays(mesh.vertices, mesh.triangles)
+    mesh = o3d.geometry.TriangleMesh(vertices=o3d.utility.Vector3dVector(vclean), triangles=o3d.utility.Vector3iVector(fclean))
     
-    #mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(point_cloud, 0.04)
-    #mesh.compute_vertex_normals()
+    # radii = [0.005, 0.01, 0.02, 0.04, 0.08]
+    # mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(point_cloud, o3d.utility.DoubleVector(radii))
     
-    #radii = [0.005, 0.01, 0.02, 0.04, 0.08]
-    #mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(point_cloud, o3d.utility.DoubleVector(radii))
+    mesh.compute_vertex_normals()
+    #o3d.visualization.draw_geometries([mesh])
+    o3d.io.write_triangle_mesh("MeshICP.stl", mesh)
+
+    ### COLLISION MESH ###
+
+    collision_offset_value = 0.01
+    collision_resolution = 0.01
+
+    collision_mesh = mrmeshpy.loadMesh("MeshICP.stl")
+
+    collision_offset_params = mrmeshpy.OffsetParameters()
+    collision_offset_params.voxelSize = collision_resolution
+
+    # (0) Unsigned : unsigned distance, useful for bidirectional Shell offset
+    # (1) OpenVDB : sign detection from OpenVDB library, which is good and fast if input geometry is closed
+    # (2) ProjectionNormal : the sign is determined based on pseudonormal in closest mesh point (unsafe in case of self-intersections)
+    # (3) WindingRule : ray intersection counter, significantly slower than ProjectionNormal and does not support holes in mesh
+    # (4) HoleWindingRule : computes winding number generalization with support of holes in mesh, slower than WindingRule 
+    collision_offset_params.signDetectionMode = mrmeshpy.SignDetectionMode.OpenVDB
+
+    collision_offset_mesh = mrmeshpy.offsetMesh(collision_mesh, collision_offset_value, collision_offset_params)
+
+    mrmeshpy.saveMesh(collision_offset_mesh,"StudiedObject.stl")
+
+    tmp_mesh = trimesh.load_mesh("StudiedObject.stl")  
+    tmp_mesh = coacd.Mesh(tmp_mesh.vertices, tmp_mesh.faces)
+    result = coacd.run_coacd(tmp_mesh,
+                             threshold=0.15,
+                             max_convex_hull=5,
+                             preprocess_mode="auto",
+                             preprocess_resolution=50,
+                             resolution=2000,
+                             mcts_nodes=20,
+                             mcts_iterations=150,
+                             mcts_max_depth=3,
+                             pca=False,
+                             merge=True)
+
+    mesh_parts = []
+    for vs, fs in result:
+        mesh_parts.append(trimesh.Trimesh(vs, fs))
+
+    scene = trimesh.Scene()
+    for p in mesh_parts:
+        scene.add_geometry(p)
+    scene.export("StudiedObject.stl")
+
+    ### CROPPING ###
+
+    mesh_centroid = np.mean(np.asarray(mesh.vertices), axis=0)
+    mesh_size = np.linalg.norm(mesh.get_max_bound()-mesh.get_min_bound())/(2*np.sqrt(3))
+
+    crop_mesh = mesh.crop(o3d.geometry.AxisAlignedBoundingBox(min_bound=[mesh_centroid[0]-mesh_size, mesh_centroid[1]-mesh_size, mesh_centroid[2]-mesh_size], max_bound=[mesh_centroid[0]+mesh_size, mesh_centroid[1]+mesh_size, mesh_centroid[2]+mesh_size]))
+
+    # o3d.visualization.draw_geometries([crop_box])
+    o3d.io.write_triangle_mesh("MeshICP_Cropped.stl", crop_mesh)
+            
+    ### OFFSETTING, SMOOTHING & DECIMATION ###
+
+    offset_value = 0.05
+    resolution = 0.05
+
+    #Step 1 : Offset surface
+
+    mesh = mrmeshpy.loadMesh("MeshICP_Cropped.stl")
+
+    offset_params = mrmeshpy.OffsetParameters()
+    offset_params.voxelSize = resolution/10  #More than expected resolution (decimate afterwards)
+
+    # (0) Unsigned : unsigned distance, useful for bidirectional Shell offset
+    # (1) OpenVDB : sign detection from OpenVDB library, which is good and fast if input geometry is closed
+    # (2) ProjectionNormal : the sign is determined based on pseudonormal in closest mesh point (unsafe in case of self-intersections)
+    # (3) WindingRule : ray intersection counter, significantly slower than ProjectionNormal and does not support holes in mesh
+    # (4) HoleWindingRule : computes winding number generalization with support of holes in mesh, slower than WindingRule 
+    offset_params.signDetectionMode = mrmeshpy.SignDetectionMode.OpenVDB
+
+    offset_mesh = mrmeshpy.offsetMesh(mesh, offset_value, offset_params)
+
+    # verts = mrmeshnumpy.getNumpyVerts(mesh)
+    # faces = mrmeshnumpy.getNumpyFaces(mesh.topology)
+
+    # display = trimesh.Trimesh(verts, faces)
+    # display.visual.vertex_colors = trimesh.visual.random_color()
+
+    # offset_verts = mrmeshnumpy.getNumpyVerts(offset_mesh)
+    # offset_faces = mrmeshnumpy.getNumpyFaces(offset_mesh.topology)
+
+    # scene = trimesh.scene.Scene()
+    # scene.add_geometry(display)
+    # offset_display = trimesh.Trimesh(offset_verts, offset_faces)
+    # offset_display.visual.vertex_colors = trimesh.visual.random_color()
+    # offset_display.visual.vertex_colors[:,-1] = 126
+    # scene.add_geometry(offset_display)
+    # scene.show()
+
+    #Step 2 : Smooth surface
+
+    smooth_params = mrmeshpy.MeshRelaxParams()
+    smooth_params.force = 0.2   #Between 0.0 and 0.5
+    smooth_params.iterations = 5
+
+    smooth_result = mrmeshpy.relaxKeepVolume(offset_mesh,smooth_params)
+
+    # smooth_verts = mrmeshnumpy.getNumpyVerts(offset_mesh)
+    # smooth_faces = mrmeshnumpy.getNumpyFaces(offset_mesh.topology)
+
+    # scene = trimesh.scene.Scene()
+    # scene.add_geometry(display)
+    # smooth_display = trimesh.Trimesh(smooth_verts, smooth_faces)
+    # smooth_display.visual.vertex_colors = trimesh.visual.random_color()
+    # smooth_display.visual.vertex_colors[:,-1] = 126
+    # scene.add_geometry(smooth_display)
+    # scene.show()
+
+    #Step 3 : Decimate surface (adapt mesh size)
     
-    o3d.visualization.draw_geometries([mesh])
+    # Setup decimate parameters
+    decimate_settings = mrmeshpy.DecimateSettings()
+    decimate_settings.strategy = mrmeshpy.DecimateStrategy.ShortestEdgeFirst
+    decimate_settings.maxError = 10e10
+    decimate_settings.maxEdgeLen = resolution
+    decimate_settings.tinyEdgeLength = resolution
+    
+    # Decimate mesh
+    decimate_result = mrmeshpy.decimateMesh(offset_mesh, decimate_settings)
+
+    # decimate_verts = mrmeshnumpy.getNumpyVerts(offset_mesh)
+    # decimate_faces = mrmeshnumpy.getNumpyFaces(offset_mesh.topology)
+
+    # scene = trimesh.scene.Scene()
+    # scene.add_geometry(display)
+    # decimate_display = trimesh.Trimesh(decimate_verts, decimate_faces)
+    # decimate_display.visual.vertex_colors = trimesh.visual.random_color()
+    # decimate_display.visual.vertex_colors[:,-1] = 126
+    # scene.add_geometry(decimate_display)
+    # scene.show(flags={'wireframe': True})
+
+    mrmeshpy.saveMesh(offset_mesh,"OffsetMesh.stl")
+
+    final_mesh = trimesh.load_mesh("OffsetMesh.stl")  
+    final_mesh.show(flags={'wireframe': True})
+
+    print("Measurements points (P1) : ", len(final_mesh.vertices))
+    print("Final resolution : ", final_mesh.edges_unique_length.max())
+
+    # Save final mesh
+    meshio.write_points_cells("MeasurementsMesh.mesh", final_mesh.vertices, [("triangle",final_mesh.faces)])
+    #final_mesh.export("StudiedObject.stl")
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
